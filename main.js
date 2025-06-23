498e604fbb6dc362001f6095786df6a6142af760
Mohamed Abo Hebil <mabuhebil@gmail.com>
2025-06-23 19:48:22 +0300

    update

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ function toCelis() {
     let output = (parseFloat(inputCelis.value) * 9 / 5) + 32;
     inputFarh.value = parseFloat(output.toFixed(2))
 }
+
+
+function toFar() {
+    let output = (parseFloat(inputFarh.value) - 32) * 5 / 9;
+    inputCelis.value = parseFloat(output.toFixed(2))
+}
